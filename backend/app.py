574861343693624217428026dce92cd64412d71e
Shaddy <shadybett540@gmail.com>
2025-02-11 19:14:00 +0300

    removed manual header modification

--- backend/app.py
+++ backend/app.py
@@ -190,7 +190,7 @@ CORS(app, resources={r"/*": {"origins": ["http://localhost:3001", "https://www.s
 def handle_options_request():
     if request.method == 'OPTIONS':
         response = make_response()
-        response.headers['Access-Control-Allow-Origin'] = 'https://www.studypage.cloud'
+        # response.headers['Access-Control-Allow-Origin'] = 'https://www.studypage.cloud'
         response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
         response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
         return response
